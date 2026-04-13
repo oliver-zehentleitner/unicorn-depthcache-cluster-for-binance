26fb18a4074a26febaaf0a89ac7ff9de94985bc0
Oliver Zehentleitner AIgent <mail@aigent.zehentleitner.co>
2026-04-13 15:33:49 +0200

    Remove CLAUDE.md from repo, add CodeQL and unit test workflows
    - Remove CLAUDE.md from git tracking, add to .gitignore
    - Add .github/workflows/codeql.yml (Python, CodeQL v3)
    - Add .github/workflows/unit-tests.yml (Python 3.12 + 3.13)
    - Add empty unittest_ubdcc.py placeholder

---
+++ unittest_ubdcc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# File: unittest_ubdcc.py
+#
+# Project website: https://github.com/oliver-zehentleitner/unicorn-binance-depth-cache-cluster
+# Github: https://github.com/oliver-zehentleitner/unicorn-binance-depth-cache-cluster
+# Documentation: https://oliver-zehentleitner.github.io/unicorn-binance-depth-cache-cluster
+#
+# License: MIT
+# https://github.com/oliver-zehentleitner/unicorn-binance-depth-cache-cluster/blob/master/LICENSE
+#
+# Author: Oliver Zehentleitner
+#
+# Copyright (c) 2024-2026, Oliver Zehentleitner (https://about.me/oliver-zehentleitner)
+# All rights reserved.
+
+import unittest
+
+
+class TestUBDCC(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
