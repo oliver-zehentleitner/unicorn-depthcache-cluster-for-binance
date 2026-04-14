e3b210eb9fb224c1f606942cb2f3fa8b25eee268
Oliver Zehentleitner AIgent <mail@aigent.zehentleitner.co>
2026-04-14 20:54:58 +0200

    Remove dev/helm/release.sh (replaced by helm_release GH Actions workflow)

--- dev/helm/release.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Package the Helm chart and regenerate the repo index.
-# Run from the repo root: bash dev/helm/release.sh
-cd ./docs/helm
-helm package ../../dev/helm/ubdcc/
-helm repo index . --url https://oliver-zehentleitner.github.io/unicorn-binance-depth-cache-cluster/helm
